996f19ca567acf6994008f1958af5837771f86cf
Kalina Udemy Course <kalina.dzhenkova@ottogroup.com>
2018-02-20 06:37:16 -0500

    Add edit update functionality and partial

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,9 @@ Rails.application.routes.draw do
   resources :articles
   
   get 'signup', to: 'users#new'
-  # resources :users, except: [:new]
-  post 'users', to: 'users#create'
+  resources :users, except: [:new]
+  # post 'users', to: 'users#create'
+  # post 'users', to: 'users#edit'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
