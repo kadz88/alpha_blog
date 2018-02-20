7241a2eb3fd7579bd18df80e79b71674f262a291
Kalina Udemy Course <kalina.dzhenkova@ottogroup.com>
2018-02-20 10:35:51 -0500

    add sessions

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,11 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to:'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   # post 'users', to: 'users#create'
   # post 'users', to: 'users#edit'
   # Example of named route that can be invoked with purchase_url(id: product.id)
