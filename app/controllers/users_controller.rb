74d2bd6f3d2190d000c68d86d0524465c7d28eee
Kalina Udemy Course <kalina.dzhenkova@ottogroup.com>
2018-02-21 05:03:48 -0500

    Add adin user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :requier_same_user, only: [:edit, :update]
+  before_action :requier_same_user, only: [:edit, :update, :destroy]
+  before_action :requier_admin, only: [:destroy]
+  
   def index
     @users = User.paginate(page: params[:page], per_page: 3)
   end
@@ -37,6 +39,13 @@ class UsersController < ApplicationController
     @user_articles =  @user.articles.paginate(page: params[:page], per_page: 2)
   end
   
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all articles created by user have been deleted"
+    redirect_to users_path
+  end
+  
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)  
@@ -47,9 +56,16 @@ class UsersController < ApplicationController
   end
   
   def requier_same_user
-    if current_user != @user
+    if current_user != @user and !current_user.admin?
       flash[:danger] = "You can only edit your own account"
       redirect_to root_path
     end
   end
+  
+  def requier_admin
+    if logged_in? and !current_user.admin?
+      flash[:danger] = "Only admin users can perform that action"
+    end
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220092346) do
+ActiveRecord::Schema.define(version: 20180221093203) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20180220092346) do
     t.datetime "updated_at"
     t.string   "password_digets"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
